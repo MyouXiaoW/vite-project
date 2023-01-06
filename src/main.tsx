414e0329c2f6537b1a682925afafd4eb7405e6ac
王圣玉 <wangshengyu@huohua.cn>
2023-01-06 18:23:44 +0800

    css预处理less

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.less'
 
 /**
  * vite 利用ES Module规范支持实现开发阶段的 Dev Server，进行模块的按需加载，而不是先整体打包再进行加载
- * 比如说import react from 'react' 相当于对vite dev server进行了一次http请求
+ * 比如说import react from 'react' 相当于对vite dev server进行了一次http请求，这样就无需进行打包了
  * 
  */
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
