db43bd7bda1a4e92c9c5977a9527d490a18b54d7
王圣玉 <wangshengyu@huohua.cn>
2023-01-06 18:43:30 +0800

    postcss后处理

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,14 @@ export default defineConfig({
    * 来提供 React 项目编译和热更新的功能。
    */
   plugins: [react()],
+  css:{
+    postcss:{
+      plugins:[
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist:['safari >= 6', 'ff >= 10']
+        })
+      ]
+    }
+  }
 })
